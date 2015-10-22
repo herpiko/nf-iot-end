8848878d6341abee73b0afcd6670988aeb37ad58
Herpiko Dwi Aguno <herpiko@gmail.com>
2015-10-22 23:11:04 +0700

    Fix typo, optional condition for hostname & port

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var clientio = require("socket.io-client");
-var client = clientio.connect(process.env.HOST + ":" + process.env.PORT);
+var hostname = process.env.HOST || "localhost";
+var port = process.env.PORT || 3001;
+var client = clientio.connect(host + ":" + port.toString());
 var Gpio = require("onoff").Gpio;
 
 var slot = {}
@@ -11,18 +13,19 @@ client.on("command", function(data){
   console.log("change " + data.key +  " status to : " + data.value);
   slot[data.key].writeSync(parseInt(data.value));
 })
+  
+client.on("message", function(data){
+  console.log(data);
+});
 
 // This is an example, the pi trigger a switch.
 setTimeout(function(){
   client.emit("switch1", 1);
-  client.on("message", function(data){
-    console.log(data);
-  });
 }, 5000)
 
+
 var exit = function(){
-  led.unexport();
+  slot.led1.unexport();
   process.exit();
 }
-
 process.on("SIGINT", exit);
