057d00383089638231012524c47c71e412a09dfd
Herpiko Dwi Aguno <herpiko@gmail.com>
2015-10-23 16:13:41 +0000

    Rejoin room when reconnect, minor fixes

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 var clientio = require("socket.io-client");
-var hostname = process.env.HOST || "localhost";
-var port = process.env.PORT || 3001;
-var client = clientio.connect(host + ":" + port.toString());
+if (!process.env.HOST || !process.env.PORT) {
+  console.log("You must specify a middleware instance's HOST and PORT");
+  process.exit();
+}
+var client = clientio.connect("http://" + process.env.HOST + ":" + process.env.PORT);
 var Gpio = require("onoff").Gpio;
 
 var slot = {}
 slot.led1 = new Gpio(14, "out");
 
+// Let it bip once
+slot.led1.writeSync(1);
+setTimeout(function(){slot.led1.writeSync(0);}, 300);
 
-client.emit("join-end");
+client.on("whoareu", function(){
+  client.emit("join-end");
+})
+
 client.on("command", function(data){
   console.log("change " + data.key +  " status to : " + data.value);
   slot[data.key].writeSync(parseInt(data.value));
